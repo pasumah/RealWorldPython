d808eb9e2bf249716bec1010ef85852f7f0b4abe
pasumah <45134023+pasumah@users.noreply.github.com>
2024-05-23 00:13:18 -0600

    Commiting new changes

---
+++ chapter2/practice_hound_dispersion.py
@@ -0,0 +1,16 @@
+"""Use NLP (nltk) to make dispersion plot."""
+import matplotlib.pyplot as plt
+from nltk.draw.dispersion import dispersion_plot
+    
+def text_to_string(filename):
+    """Read a text file and return a string."""
+    with open(filename) as infile:
+        return infile.read()
+
+    corpus = text_to_string('hound.txt')
+    tokens = nltk.word_tokenize(corpus)
+    tokens = nltk.Text(tokens)  # NLTK wrapper for automatic text analysis.
+    words = ['Holmes', 'Watson', 'Mortimer', 'Henry', 'Barrymore', 'Stapleton', 'Selden', 'hound']
+    ax = dispersion_plot(tokens, words)
+    # Correct current bug in NLTK dispersion_plot that reverses label order by mistake:
+    ax.set_yticks(list(range(len(words))), reversed(words), color="C0")
